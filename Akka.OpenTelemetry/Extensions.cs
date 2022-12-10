57527eab02d811408221a390dd038a3ad9a54065
Roger Johansson <roger@asynkron.se>
2022-12-10 06:45:12 +0100

    .

--- Akka.OpenTelemetry/Extensions.cs
+++ Akka.OpenTelemetry/Extensions.cs
@@ -1,10 +1,14 @@
 using Akka.Actor;
 using Akka.OpenTelemetry;
+using OpenTelemetry.Trace;
 
 namespace Akka;
 
 public static class TypeExtensions
 {
+    public static TracerProviderBuilder AddAkkaInstrumentation(this TracerProviderBuilder builder) =>
+        builder.AddSource(OtelTags.ActivitySourceName);
+
     public static string GetMessageTypeName(this object? message)
     {
         return message?.GetType().Name ?? "null";
---
+++ Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// create a new actor system
+
+using Akka;
+using Akka.Actor;
+using Akka.Configuration;
+
+var bootstrap = BootstrapSetup.Create().WithConfig(
+    ConfigurationFactory.ParseString("""
+akka.actor.provider = "Akka.OpenTelemetry.OpenTelemetryActorRefProvider, Akka.OpenTelemetry"
+"""));
+
+var system = ActorSystem.Create("my-system", bootstrap);
+var props = Props.Create<MyActor>().WithTracing();
+var reff = system.ActorOf(props);
+
+reff.Tell("Testing");
+
+Console.ReadLine();
+
+
+
+
+
+class MyActor : UntypedActor
+{
+    protected override void OnReceive(object message)
+    {
+        if (message is string s)
+        {
+            Console.WriteLine("Got message string: " + s);
+        }
+    }
+}
