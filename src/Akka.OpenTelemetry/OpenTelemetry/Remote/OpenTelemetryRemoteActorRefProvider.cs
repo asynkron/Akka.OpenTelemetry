33ad578748a6a124b748fcee2bc65c001d3a3084
Roger Johansson <roger@asynkron.se>
2022-12-11 18:21:51 +0100

    reuse spawner logic

--- src/Akka.OpenTelemetry/OpenTelemetry/Remote/OpenTelemetryRemoteActorRefProvider.cs
+++ src/Akka.OpenTelemetry/OpenTelemetry/Remote/OpenTelemetryRemoteActorRefProvider.cs
@@ -29,7 +29,7 @@ public sealed class OpenTelemetryRemoteActorRefProvider: RemoteActorRefProviderD
         ActorPath path,
         bool systemService, Deploy deploy, bool lookupDeploy, bool async)
     {
-        if (NotTraced(props, systemService))
+        if (Spawner.NotTraced(props, systemService))
         {
             Console.WriteLine("Not traced " + path);
             return base.ActorOf(system, props, supervisor, path, systemService, deploy, lookupDeploy, async);
@@ -37,53 +37,12 @@ public sealed class OpenTelemetryRemoteActorRefProvider: RemoteActorRefProviderD
 
         //TODO: figure out what to do with remote deployments here...
 
-        return LocalActorOf(system, props, supervisor, path, deploy, lookupDeploy, async);
+        //reuse the spawn logic
+        return Spawner.LocalActorOf(system, props, supervisor, path, deploy, lookupDeploy, async);
     }
 
-    private static bool NotTraced(Props props, bool systemService)
-    {
-        return systemService || props.Deploy is not OpenTelemetryDeploy;
-    }
+
 
 
     //Random copy paste from LocalActorRefProvider
-    private IInternalActorRef LocalActorOf(ActorSystemImpl system, Props props, IInternalActorRef supervisor,
-        ActorPath path, Deploy deploy, bool lookupDeploy, bool async)
-    {
-        var props2 = props;
-        var propsDeploy = lookupDeploy ? Deployer.Lookup(path) : deploy;
-        if (propsDeploy != null)
-        {
-            if (propsDeploy.Mailbox != Deploy.NoMailboxGiven)
-                props2 = props2.WithMailbox(propsDeploy.Mailbox);
-            if (propsDeploy.Dispatcher != Deploy.NoDispatcherGiven)
-                props2 = props2.WithDispatcher(propsDeploy.Dispatcher);
-        }
-
-        if (!system.Dispatchers.HasDispatcher(props2.Dispatcher))
-        {
-            throw new ConfigurationException($"Dispatcher [{props2.Dispatcher}] not configured for path {path}");
-        }
-
-        try
-        {
-            // for consistency we check configuration of dispatcher and mailbox locally
-            var dispatcher = _system.Dispatchers.Lookup(props.Dispatcher);
-            var mailboxType = _system.Mailboxes.GetMailboxType(props, dispatcher.Configurator.Config);
-
-            var settings = new OpenTelemetrySettings(true);
-            return async switch
-            {
-                true => new OpenTelemetryRepointableActorRef(settings, system, props2, dispatcher, mailboxType, supervisor,
-                    path).Initialize(async),
-                _ => new OpenTelemetryLocalActorRef(settings, system, props, dispatcher, mailboxType, supervisor, path)
-            };
-        }
-        catch (Exception ex)
-        {
-            throw new ConfigurationException(
-                $"Configuration problem while creating [{path}] with dispatcher [{props.Dispatcher}] and mailbox [{props.Mailbox}]",
-                ex);
-        }
-    }
 }
---
+++ src/Akka.OpenTelemetry/Spawner.cs
@@ -0,0 +1,56 @@
+using Akka.Actor;
+using Akka.Actor.Internal;
+using Akka.Configuration;
+using Akka.OpenTelemetry;
+using Akka.OpenTelemetry.Local.ActorRefs;
+
+namespace Akka;
+
+public static class Spawner
+{
+    public static bool NotTraced(Props props, bool systemService)
+    {
+        return systemService || props.Deploy is not OpenTelemetryDeploy;
+    }
+
+    public static IInternalActorRef LocalActorOf (ActorSystemImpl system, Props props, IInternalActorRef supervisor,
+        ActorPath path, Deploy deploy, bool lookupDeploy, bool async)
+    {
+
+        var props2 = props;
+        var propsDeploy = lookupDeploy ? system.Provider.Deployer.Lookup(path) : deploy;
+        if (propsDeploy != null)
+        {
+            if (propsDeploy.Mailbox != Deploy.NoMailboxGiven)
+                props2 = props2.WithMailbox(propsDeploy.Mailbox);
+            if (propsDeploy.Dispatcher != Deploy.NoDispatcherGiven)
+                props2 = props2.WithDispatcher(propsDeploy.Dispatcher);
+        }
+
+        if (!system.Dispatchers.HasDispatcher(props2.Dispatcher))
+        {
+            throw new ConfigurationException($"Dispatcher [{props2.Dispatcher}] not configured for path {path}");
+        }
+
+        try
+        {
+            // for consistency we check configuration of dispatcher and mailbox locally
+            var dispatcher = system.Dispatchers.Lookup(props.Dispatcher);
+            var mailboxType = system.Mailboxes.GetMailboxType(props, dispatcher.Configurator.Config);
+
+            var settings = new OpenTelemetrySettings(true);
+            return async switch
+            {
+                true => new OpenTelemetryRepointableActorRef(settings, system, props2, dispatcher, mailboxType, supervisor,
+                    path).Initialize(async),
+                _ => new OpenTelemetryLocalActorRef(settings, system, props, dispatcher, mailboxType, supervisor, path)
+            };
+        }
+        catch (Exception ex)
+        {
+            throw new ConfigurationException(
+                $"Configuration problem while creating [{path}] with dispatcher [{props.Dispatcher}] and mailbox [{props.Mailbox}]",
+                ex);
+        }
+    }
+}
